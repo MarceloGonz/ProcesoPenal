57bc78dec22749afc82c68b832dd978c1b877950
MarjanCs <marjanazliga@gmail.com>
2022-07-08 09:45:34 -0500

    eliminacion pagina personas

--- Proceso Penal/Backend/API.py
+++ Proceso Penal/Backend/API.py
@@ -12,8 +12,9 @@ origins = [
     
     "http://localhost",
     "http://localhost:8080",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1",
     "http://127.0.0.1:5500"
-    "http://127.0.0.1"
 ]
 
 app.add_middleware(
